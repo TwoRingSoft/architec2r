ed05be6e03bc43c6ef3ef6e1185424b7f56f030a
Andrew McKnight <amcknight2718@gmail.com>
2019-03-01 14:53:41 -0900

    initial commit

---
+++ Application/Application.h
@@ -0,0 +1,19 @@
+//
+//  Application.h
+//  Application
+//
+//  Created by Andrew McKnight on 1/24/19.
+//  Copyright © 2019 Two Ring Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Application.
+FOUNDATION_EXPORT double ApplicationVersionNumber;
+
+//! Project version string for Application.
+FOUNDATION_EXPORT const unsigned char ApplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Application/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Architec2r
+
+Expressions of various application architecture paradigms in Swift code, mostly protocols.
